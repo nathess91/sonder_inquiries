3916d0db5b1a6f24da51d7a31f80abdfcae69485
Natalia Hess <nathess91@gmail.com>
2017-07-17 20:25:08 -0700

    Creates functionality for making a new inquiry

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,2 +1,31 @@
 class InquiriesController < ApplicationController
+  def index
+    @inquiries = Inquiry.all
+  end
+
+  def show
+    @inquiry = Inquiry.find(params[:id])
+  end
+
+  def new
+    @unit = Unit.find(params[:unit_id])
+    @inquiry = Inquiry.new
+  end
+
+  def create
+    @inquiry = Unit.find(params[:unit_id]).inquiries.create(inquiry_params)
+
+    if @inquiry.valid?
+      redirect_to root_path
+    else
+      redirect_to :back
+    end
+
+  end
+
+  private
+
+  def inquiry_params
+    params.require(:inquiry).permit(:rental_site, :phone_number, :email, :first_name, :last_name, :arrival_date, :departure_date, :unit_id)
+  end
 end
