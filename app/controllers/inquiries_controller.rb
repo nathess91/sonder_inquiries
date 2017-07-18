a1803cde0b8e34359cd12a8a31a8deb717f8dcf0
Natalia Hess <nathess91@gmail.com>
2017-07-18 12:19:14 -0700

    Adds flash messages for inquiry

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -15,11 +15,14 @@ class InquiriesController < ApplicationController
   end
 
   def create
+    @unit = Unit.find(params[:unit_id])
     @inquiry = Unit.find(params[:unit_id]).inquiries.create(inquiry_params)
 
     if @inquiry.valid?
-      redirect_to root_path
+      flash[:success] = "Inquiry created successfully"
+      redirect_to unit_path(@unit.id)
     else
+      flash[:error] = @inquiry.errors.full_messages.join(". ")
       redirect_to :back
     end
 
--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,18 +1,4 @@
 class Inquiry < ApplicationRecord
   belongs_to :unit, inverse_of: :inquiries
 
-  # after_initialize :price, unless: :persisted?
-  # after_initialize :taxes_withheld, unless: :persisted?
-  #
-  # def self.calculate_price
-  #   # if person makes reservation on dates found within Unit.first.day_prices use that value
-  #   # else use avg cost value
-  #   # unit nightly cost * days rented
-  #
-  # end
-  #
-  # def self.calculate_price_taxes_withheld
-  #   # unit nightly cost * tax rate * days rented
-  # end
-
 end
