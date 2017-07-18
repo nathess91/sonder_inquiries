af142e36367ff56f08b9cf60940f3c185f149c6a
Natalia Hess <nathess91@gmail.com>
2017-07-18 11:02:32 -0700

    Adds logic to calculate total price and taxes withheld

--- app/helpers/inquiries_helper.rb
+++ app/helpers/inquiries_helper.rb
@@ -8,22 +8,24 @@ module InquiriesHelper
 
   def calculate_total_price(inquiry_id, unit_id)
     inquiry = Inquiry.find_by_id(inquiry_id)
-    # get all in-between reservation dates
-    inquiry_date_range = (inquiry.arrival_date..inquiry.departure_date).map(&:to_s)
-    day_price_dates = Unit.find_by_id(unit_id).day_prices.pluck(:date, :price).map(&:to_s)
+    day_prices = Unit.find_by_id(unit_id).day_prices.pluck(:date, :price)
+    total_price = 0
 
-    day_price_dates.each do |date, price|
-      if inquiry_date_range.include?(date)
-        return price
+    day_prices.each do |date, price|
+      if (inquiry.arrival_date...inquiry.departure_date).cover?(date)
+        total_price += price
       else
-        puts "nope"
+        total_price = get_avg_price(unit_id) * total_nights_rented(inquiry_id)
       end
     end
 
+    return total_price.to_f
+
   end
 
-end
+  def calculate_taxes(inquiry_id, unit_id)
+    unit_tax_rate = Unit.find_by_id(unit_id).tax_percent
+    (calculate_total_price(inquiry_id, unit_id) * unit_tax_rate).to_f
+  end
 
-# if person makes reservation on dates found within Unit.first.day_prices use that value
-# else use avg cost value
-# unit nightly cost * days rented
+end # end InquiriesHelper
